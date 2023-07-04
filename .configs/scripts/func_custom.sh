7e213c0d627c7725f5eb04e1e39a6ad07be77c8a
someone5678 <nemui3353@gmail.com>
2023-07-05 04:41:04 +0900

    Adapt to TheParasiteProject
    Change-Id: I5a9df2c94747dd7ebdd8173149a37e45a47ed298

--- .configs/scripts/func_custom.sh
+++ .configs/scripts/func_custom.sh
@@ -9,9 +9,45 @@ repo_sync_custom () {
 }
 
 update_repo_manifest () {
-    return
+	# Update sync (and sync again failed sync)
+	cd $ROMBASE/.repo/manifests
+	git reset --hard
+	git clean -f
+	git pull 2>/dev/null
+	if [ $? != 0 ];
+	then
+		if [ "$FORCEINIT" = true ];
+		then
+			cd $ROMBASE
+			echo "git failed to update manifest. Do repo init again..."
+			repo_init
+		fi
+	fi
 }
 
 build_custom () {
-    return
+	lunch $LUNCHCOMMAND
+	
+	if [ "$BOOTIMG" = true ];
+	then
+		buildtarget="bootimage dtboimage"
+		buildout="boot.img;dtbo.img"
+		outdir="$OUTDIR"
+		build_and_copy "$buildtarget" "$buildout" "$outdir"
+	elif [ "$BUILDDIST" = true ];
+	then
+		buildtarget="dist"
+		buildout="$ROMNAME*$DEVICE*.zip"
+		outdir="$OUTDIR"
+		build_and_copy "$buildtarget" "$buildout" "$outdir"
+	else
+		buildtarget="bacon"
+		buildout="$ROMNAME*$DEVICE*.zip"
+		outdir="$OUTDIR"
+		build_and_copy "$buildtarget" "$buildout" "$outdir"
+	fi
+	
+	buildtarget=
+	buildout=
+	outdir=
 }
